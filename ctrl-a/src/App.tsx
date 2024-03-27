12fc7b811c50450b14a75fc21ce175950b1ba74d
Mariana Izumi <mariana.izumiks@gmail.com>
2024-03-26 22:28:01 -0300

    5 - Preparando para a tarefa

--- ctrl-a/src/App.tsx
+++ ctrl-a/src/App.tsx
@@ -1,19 +1,20 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Keep from './pages/keep';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ } />
-        <Route path="/example/com" element={ } />
+        <Route path="/" element={ < Keep /> } />
+        {/* <Route path="/example/com" element={ } />
         <Route path="/example/com" element={ } />
         <Route path="/funcionario" element={ }>
           <Route index element={ } />
           <Route path="example" element={ } />
-        </Route>
+        </Route> */}
         {/* path="*" serve para qualquer rota, então deve ficar por último e direcionar para a home ou uma página de erro 404 */}
-        <Route path="*" element={ } />
+        <Route path="*" element={ < Keep /> } />
       </Routes>
     </BrowserRouter>
   );
---
+++ ctrl-a/src/pages/keep.tsx
@@ -0,0 +1,8 @@
+export default function Keep() {
+    return (
+        <>
+            oi
+        </>
+    )
+
+}
