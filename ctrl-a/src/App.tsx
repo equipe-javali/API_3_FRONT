5dcde3a50980aaef406e014104d976884bedc9d6
BrunoSerpa <serpabruno2005@gamail.com>
2024-04-01 08:56:08 -0300

    1 - Inseri o caminho do cadastro de ativos

--- ctrl-a/src/App.tsx
+++ ctrl-a/src/App.tsx
@@ -1,19 +1,20 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CadastroAtivosTangiveis from './pages/cadastroAtivosTangiveis';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ } />
-        <Route path="/example/com" element={ } />
-        <Route path="/example/com" element={ } />
-        <Route path="/funcionario" element={ }>
-          <Route index element={ } />
-          <Route path="example" element={ } />
+        <Route path="/" element={<CadastroAtivosTangiveis/>} />
+        <Route path="/example/com" element={<CadastroAtivosTangiveis/>} />
+        <Route path="/example/com" element={<CadastroAtivosTangiveis/>} />
+        <Route path="/funcionario" element={<CadastroAtivosTangiveis/>}>
+          <Route index element={<CadastroAtivosTangiveis/>} />
+          <Route path="example" element={<CadastroAtivosTangiveis/>} />
         </Route>
         {/* path="*" serve para qualquer rota, então deve ficar por último e direcionar para a home ou uma página de erro 404 */}
-        <Route path="*" element={ } />
+        <Route path="*" element={<CadastroAtivosTangiveis/>} />
       </Routes>
     </BrowserRouter>
   );
