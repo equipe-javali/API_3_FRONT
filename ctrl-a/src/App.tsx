0a1a42214fc970c6ed9264d12e64d0614469c3ec
BrunoSerpa <serpabruno2005@gmail.com>
2024-04-05 09:59:20 -0300

    2.1 - Arrumei o novo chamado das rotas

--- ctrl-a/src/App.tsx
+++ ctrl-a/src/App.tsx
@@ -1,20 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CadastroAtivosTangiveis from './pages/cadastroAtivosTangiveis';
+import CadastroAtivos from './pages/cadastroAtivos';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CadastroAtivosTangiveis/>} />
-        <Route path="/example/com" element={<CadastroAtivosTangiveis/>} />
-        <Route path="/example/com" element={<CadastroAtivosTangiveis/>} />
-        <Route path="/funcionario" element={<CadastroAtivosTangiveis/>}>
-          <Route index element={<CadastroAtivosTangiveis/>} />
-          <Route path="example" element={<CadastroAtivosTangiveis/>} />
+        <Route path="/" element={<CadastroAtivos/>} />
+        <Route path="/CadastroAtivos" element={<CadastroAtivos/>} />
+        <Route path="/funcionario" element={<CadastroAtivos/>}>
+          <Route index element={<CadastroAtivos/>} />
+          <Route path="example" element={<CadastroAtivos/>} />
         </Route>
         {/* path="*" serve para qualquer rota, então deve ficar por último e direcionar para a home ou uma página de erro 404 */}
-        <Route path="*" element={<CadastroAtivosTangiveis/>} />
+        <Route path="*" element={<CadastroAtivos/>} />
       </Routes>
     </BrowserRouter>
   );
