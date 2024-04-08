c8e9e15efaf20ff49759621751199692cd360454
Katiane <katiane.soares@fatec.sp.gov.br>
2024-04-08 14:23:13 -0300

    resolvendo conflitos no css

--- ctrl-a/src/App.tsx
+++ ctrl-a/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardAtivos from './pages/dashboardAtivos';
-import CriarUsuario from './pages/VisualizarUsuario';
+import VisualizarUsuario from './pages/visualizarUsuario';
 
 
 function App(): JSX.Element {
@@ -11,7 +11,7 @@ function App(): JSX.Element {
     <BrowserRouter>
       <Routes>
         <Route path="/ativos" element={ < DashboardAtivos /> } />
-        <Route path="/cadastrousuario" element={ < CriarUsuario /> } />
+        <Route path="/ListaUsuarios" element={ < VisualizarUsuario /> } />
         {/* path="*" serve para qualquer rota, então deve ficar por último e direcionar para a home ou uma página de erro 404 */}
         {/* <Route path="*" element={ < DashboardAtv /> } /> */}
       </Routes>
