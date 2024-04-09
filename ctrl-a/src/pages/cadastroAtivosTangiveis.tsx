c2f9afa2372eca690c4739c93bbe367d571fb569
BrunoSerpa <serpabruno2005@gmail.com>
2024-04-09 10:39:47 -0300

    2.3 - Arrumei a estrutura dos ativos tangíveis

--- ctrl-a/src/pages/cadastroAtivosTangiveis.tsx
+++ ctrl-a/src/pages/cadastroAtivosTangiveis.tsx
@@ -1,46 +1,35 @@
 import CampoAtivoPadrao from '../components/CampoAtivoPadrao';
 
-function CadastroAtivosTangiveis1() {
-    const tag = CampoAtivoPadrao("", "", "", "")
-    const garantia = CampoAtivoPadrao("", "", "", "")
-    const importancia = CampoAtivoPadrao("", "", "", "")
-    //const anexo = CampoAtivoPadrao("", "", "", "")
+export default function CadastroAtivosTangiveis() {
+    const tag = CampoAtivoPadrao("Tag", "text", "Insira as tags...")
+    const importancia = CampoAtivoPadrao("Grau de importância", "text", "Insira o grau de importância...")
+    const garantia = CampoAtivoPadrao("Validade de garantia", "date", "dd/mm/aaaa")
+    // const anexo = CampoAtivoPadrao("Anexo", "file", "insira o anexo")
+    const periodoDepreciacao = CampoAtivoPadrao("Período de depreciação", "text", "anos, meses...")
+    const taxaDepreciacao = CampoAtivoPadrao("Taxa de depreciação", "number", "00%")
     return {
         'dados': {
             "tag": tag.dados,
             "garantia": garantia.dados,
             //"anexo": anexo.dados,
-            "importancia": importancia
-        },
-        'código': (
-            <>
-                {tag.codigo}
-                {garantia.codigo}
-                {/* anexo.codigo */}
-                {importancia.codigo}
-            </>
-        )
-    }
-}
-
-function CadastroAtivosTangiveis2() {
-    const periodoDepreciacao = CampoAtivoPadrao("", "", "", "")
-    const taxaDepreciacao = CampoAtivoPadrao("", "", "", "")
-    const validade = CampoAtivoPadrao("", "", "", "")
-    return {
-        'dados': {
+            "importancia": importancia,
             "periodoDepreciacao": periodoDepreciacao.dados,
             "taxaDepreciacao": taxaDepreciacao.dados,
-            "validade": validade.dados,
         },
         'código': (
             <>
-                {periodoDepreciacao.codigo}
-                {taxaDepreciacao.codigo}
-                {validade.codigo}
+
+                <div className='colunaFormsAtivo'>
+                    {tag.codigo}
+                    {garantia.codigo}
+                    {/* anexo.codigo */}
+                </div>
+                <div className='colunaFormsAtivo'>
+                    {importancia.codigo}
+                    {periodoDepreciacao.codigo}
+                    {taxaDepreciacao.codigo}
+                </div>
             </>
         )
     }
 }
-
-export { CadastroAtivosTangiveis1, CadastroAtivosTangiveis2 }
