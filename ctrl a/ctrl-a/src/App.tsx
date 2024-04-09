0f27333c15fcae6d6dbd95d0a2c69be9127c2152
Silmara Bittencourt <silmarabittencourt13@gmail.com>
2024-04-09 10:59:20 -0300

    correção de nome da pasta para o merge

--- ctrl a/ctrl-a/src/App.tsx
+++ ctrl a/ctrl-a/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './css/criarUsuario.css';
-import CriarUsuario from './pages/criarUsuario/CriarUsuario';
+import CriarUsuario from './pages/CriarUsuario';
 
 function App(): JSX.Element {
   return (
