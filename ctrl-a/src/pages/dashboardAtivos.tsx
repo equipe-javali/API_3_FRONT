c2b300b8cfed9d67ef25d7f8adb083131d02a1d4
Marcus Betti <marcusbetti@gmail.com>
2024-04-12 10:50:01 -0300

    Conexão deletar,, listagem de ativos e ajustando associar responsavel

--- ctrl-a/src/pages/dashboardAtivos.tsx
+++ ctrl-a/src/pages/dashboardAtivos.tsx
@@ -5,11 +5,10 @@ import Modal from '../components/modal/modal';
 type AtivoProps = {
     id: number;
     nome: string;
-    responsavel: string;
+    idResponsavel: UsuarioProps;
     tipo: string;
     status: string;
     local: string;
-    atribuirResponsavel: (usuarioId: number, ativoId: number) => void; 
     excluirAtivo: (ativoId: number) => void; 
 }
 
@@ -19,28 +18,38 @@ type UsuarioLoginProps = {
     
   }
 
-  
-
 type UsuarioProps = {
-    id: number;
+id: number;
   nome: string;
   email: string;
   cpf: string;
   nascimento: string;
   departamento: string;
   telefone: string;  
-  ativos: AtivoProps[];
   usuariologin: UsuarioLoginProps[];
 }
 
 type TabelaAtivosProps<T extends AtivoProps> = {
     ativos: T[];
-    atribuirResponsavel: (usuarioId: number, ativoId: number) => void;
     excluirAtivo: (ativoId: number) => void;
 }
-function LinhaAtivo({ id, nome, responsavel, tipo, status, local, excluirAtivo } : AtivoProps) {
+
+
+function LinhaAtivo({ id, nome, idResponsavel, tipo, status, local, excluirAtivo } : AtivoProps) {
     const [showModal, setShowModal] = useState<boolean>(false);
     const [usuarios, setUsuarios] = useState<UsuarioProps[]>([]);
+    const [isHovered, setIsHovered] = useState(false);
+    // const respAtivo 
+    const localAtivo = (
+        idResponsavel.departamento === '' ? <button type='button' className='btnAtribuir' onClick={toggleModal}>Atribuir</button> : idResponsavel.departamento
+        
+    )     //(
+    //     <>
+    //         <button type='button' className='btnAtribuir' onClick={toggleModal}>Atribuir</button>
+    //         {isHovered && <button type='button' className='btnAtribuir' onClick={handleExcluir}>Excluir</button>}
+    //     </>
+    // );
+    
 
     useEffect(() => {
         fetch('http://localhost:8080/usuario/listagemTodos')
@@ -53,27 +62,20 @@ function LinhaAtivo({ id, nome, responsavel, tipo, status, local, excluirAtivo }
             .then(data => setUsuarios(data))
             .catch(error => console.error('Error:', error));
     }, []);
-    
-    function toggleModal() {
-        setShowModal(!showModal);    }
-    const [isHovered, setIsHovered] = useState(false);
-    const respAtivo = responsavel === '' ? 'Não definido' : responsavel
+
+    function toggleModal() {setShowModal(!showModal);}
+
     function handleExcluir() {
         excluirAtivo(id);
     }
-    const localAtivo = (
-        <>
-            <button type='button' className='btnAtribuir' onClick={toggleModal}>Atribuir</button>
-            {isHovered && <button type='button' className='btnAtribuir' onClick={handleExcluir}>Excluir</button>}
-        </>
-    );
+    
     
     let statusA = status
-    if (local == '' && responsavel == '') {
+    if (idResponsavel.departamento == '') {
         statusA = 'Não alocado'
         
     }
-    else if (local == 'TI') {
+    else if (idResponsavel.departamento == 'TI') {
         statusA = 'Em manutenção'
     } else {
         statusA = 'Em uso'
@@ -88,7 +90,7 @@ function LinhaAtivo({ id, nome, responsavel, tipo, status, local, excluirAtivo }
              onMouseLeave={() => setIsHovered(false)}>
             <p className="id">{id}</p>
             <p className="nome">{nome}</p>
-            <p className="responsavel">{respAtivo}</p>
+            <p className="responsavel">{idResponsavel.nome}</p>
             <p className="tipo">{tipo}</p>
             <p className="status">{statusA}</p>
             <p className="local">{localAtivo}</p>
@@ -115,18 +117,17 @@ function LinhaAtivo({ id, nome, responsavel, tipo, status, local, excluirAtivo }
 }
 
 
-function TabelaAtivos({ ativos, atribuirResponsavel, excluirAtivo } : TabelaAtivosProps<AtivoProps>) {
+function TabelaAtivos({ ativos, excluirAtivo } : TabelaAtivosProps<AtivoProps>) {
     const linhas = ativos.map((atv) => {
         return (
             <LinhaAtivo
                 key={atv.id}
                 id={atv.id}
                 nome={atv.nome}
-                responsavel={atv.responsavel}
+                idResponsavel={atv.idResponsavel}
                 tipo={atv.tipo}
                 status={atv.status}
                 local={atv.local}
-                atribuirResponsavel={atribuirResponsavel}
                 excluirAtivo={excluirAtivo} /> // Adicione esta linha
         );
     });
@@ -165,45 +166,7 @@ export default function DashboardAtivos() {
       }, []);
 
 
-      function atribuirResponsavel(usuarioId: number, ativoId: number) {
-        fetch(`http://localhost:8080/ativo/associarAtivo/${ativoId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(usuarioId),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            return fetch(`http://localhost:8080/usuario/${usuarioId}`)
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(usuario => {
-            const updatedAtivos = ativos.map(ativo => {
-                if (ativo.id === ativoId) {
-                    return {
-                        ...ativo,
-                        responsavel: usuario.nome
-                    };
-                }
-                return ativo;
-            });
-            setAtivos(updatedAtivos);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-    }
+      
 
     function excluirAtivo(ativoId: number) {
         fetch(`http://localhost:8080/ativo/exclusao/${ativoId}`, { 
@@ -236,7 +199,7 @@ export default function DashboardAtivos() {
                 </select>
                 <input/>
             </div>
-            <TabelaAtivos ativos={ativos} atribuirResponsavel={atribuirResponsavel} excluirAtivo={excluirAtivo} />
+            <TabelaAtivos ativos={ativos} excluirAtivo={excluirAtivo} />
         </div>
     );
 };
