62337932e2ff143da1846ff300b19b4fd97e47e0
Silmara <silmarabittencourt13@gmail.com>
2024-04-14 12:40:03 -0300

    Alteração nos icons do menu

--- ctrl-a/src/pages/BaseLateralHeader.tsx
+++ ctrl-a/src/pages/BaseLateralHeader.tsx
@@ -3,8 +3,11 @@ import { Outlet, RouteProps } from "react-router-dom";
 import Header from "../components/Header";
 import SideMenu from "../components/SideMenu";
 import "./css/BaseLateralHeader.css";
-import user_png from "../assets/icons/user.png";
+import criar_Ativo_png from "../assets/icons/criar_ativo.png";
+import ver_Ativo_png from "../assets/icons/ver_ativos.png";
 import home_png from "../assets/icons/home.png";
+import criar_usuario_png from "../assets/icons/criar_usuario.png";
+import visualizar_usuario_png from "../assets/icons/visualizar_usuario.png";
 
 export default class BaseLateralHeader extends Component<RouteProps> {
     render() {
@@ -12,10 +15,10 @@ export default class BaseLateralHeader extends Component<RouteProps> {
             <div id="app-content-lr">
                 <SideMenu links={[
                     [home_png, "/"],
-                    [user_png, "ListaUsuarios"],
-                    [user_png, "CadastroAtivo"],
-                    [user_png, "CadastroUsuario"],
-                    [user_png, "ListaAtivos"]
+                    [visualizar_usuario_png, "ListaUsuarios"],
+                    [criar_Ativo_png, "CadastroAtivo"],
+                    [criar_usuario_png, "CadastroUsuario"],
+                    [ver_Ativo_png, "ListaAtivos"]
                 ]} />
                 <div id="app-content-tb">
                     <Header />
