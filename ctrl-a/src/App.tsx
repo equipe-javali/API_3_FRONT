5b88adc43d061a2f54322f45e5d0fb2fd3d12921
Katiane <katiane.soares@fatec.sp.gov.br>
2024-04-16 16:14:53 -0300

    inicio do front-end e da estrutura da pagina que lista a manutencao de 1 ativo

--- ctrl-a/src/App.tsx
+++ ctrl-a/src/App.tsx
@@ -5,6 +5,7 @@ import CadastroAtivos from './pages/cadastroAtivos';
 import DashboardAtivos from './pages/dashboardAtivos';
 import CriarUsuario from './pages/criarUsuario';
 import VisualizarUsuario from './pages/VisualizarUsuario';
+import HistoricoManutencao from './pages/historicoManutencaoAtivo';
 
 
 export default function App() {
@@ -13,11 +14,12 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<BaseLateralHeader />}>
-          <Route index element={<DashboardAtivos/>} />
+          <Route index element={<HistoricoManutencao/>} />
           <Route path="/ListaAtivos" element={< DashboardAtivos />} />
           <Route path="/ListaUsuarios" element={< VisualizarUsuario />} />
           <Route path="/CadastroAtivo" element={< CadastroAtivos />} />
           <Route path="/CadastroUsuario" element={< CriarUsuario />} />
+          <Route path="/HistoricoManutencao" element={< HistoricoManutencao />} />
           {/* path="*" serve para qualquer rota, então deve ficar por último e direcionar para a home ou uma página de erro 404 */}
           <Route path="*" element={<CadastroAtivos />} />
         </Route>
