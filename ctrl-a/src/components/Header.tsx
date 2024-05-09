25f797e8bd311331810c274089646b413958e35e
BrunoSerpa <serpabruno2005@gmail.com>
2024-05-08 22:24:50 -0300

    GERAL - Inseri o logout; arrumei o perfil

--- ctrl-a/src/components/Header.tsx
+++ ctrl-a/src/components/Header.tsx
@@ -2,6 +2,8 @@ import { Component } from "react";
 import "./css/Header.css";
 import user_png from "../assets/icons/user.png";
 import logo from '../assets/icons/logo.png';
+import logout from '../assets/icons/logout.svg';
+import { Link } from "react-router-dom";
 
 
 export default class Header extends Component<{}> {
@@ -10,11 +12,17 @@ export default class Header extends Component<{}> {
             <header id="header-component">
                 <img src={logo} alt="Logo" className="logo" />
                 <div id="header-right-side">
-                    {/* <Logout/> */}
+                    <Link to="/" onClick={() => {localStorage.clear()}}>
+                        <div id="logout-icon-holder">
+                            <img src={logout} alt="Ícone de Logout" />
+                        </div>
+                    </Link>
                     {/* <Notificacao/> */}
-                    <div id="user-icon-holder">
-                        <img src={user_png} alt="Ícone de usuário" />
-                    </div>
+                    <Link to={`/EdicaoUsuario/${localStorage.getItem("id")}`}>
+                        <div id="user-icon-holder">
+                            <img src={user_png} alt="Ícone de usuário" />
+                        </div>
+                    </Link>
                 </div>
             </header>
         );
