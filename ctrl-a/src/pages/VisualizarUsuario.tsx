2ea2ef6d69b3de0b281bcda1c1aea2bffcd889b0
Katiane <katiane.soares@fatec.sp.gov.br>
2024-05-23 18:08:26 -0300

    BUG - mostrando usuarios ativos e inativos

--- ctrl-a/src/pages/VisualizarUsuario.tsx
+++ ctrl-a/src/pages/VisualizarUsuario.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './css/visualizarUsuario.css'
+import './css/visualizarUsuario.css';
 import RespostaSistema from '../components/respostaSistema';
 import { FaPencilAlt, FaTrash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
@@ -23,7 +23,7 @@ interface Usuario {
   nascimento: string;
   departamento: string;
   telefone: string;
-  status: string;
+  status: string; // Status como string
   ativos: Ativo[];
   usuariologin: UsuarioLogin;
 }
@@ -31,14 +31,15 @@ interface Usuario {
 export default function VisualizarUsuario() {
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
   const [departamentos, setDepartamentos] = useState<string[]>([]);
-  const [Pesquisa, setFilterValue] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
-  const [textoResposta, setTextoResposta] = useState('')
-  const [tipoResposta, setTipoResposta] = useState('')
-  function fechaPopUp() {
-    setTextoResposta('')
-    setTipoResposta('')
-  }
+  const [filtroDepartamento, setFiltroDepartamento] = useState('');
+  const [termoBusca, setTermoBusca] = useState('');
+  const [textoResposta, setTextoResposta] = useState('');
+  const [tipoResposta, setTipoResposta] = useState('');
+  const [mostrarInativos, setMostrarInativos] = useState(true);
+  const [chaveAtualizacao, setChaveAtualizacao] = useState(0);
+
+  const token = getLocalToken();
+
   useEffect(() => {
     if (tipoResposta === "Sucesso") {
       const timer = setTimeout(() => {
@@ -48,61 +49,44 @@ export default function VisualizarUsuario() {
     }
   }, [tipoResposta]);
 
-  const token = getLocalToken();
-
   useEffect(() => {
     fetch('http://localhost:8080/usuario/listagemTodos', {
-      headers: {
-        "Authorization": token
-      }
+      headers: { "Authorization": token }
     })
-      .then((response) => {
+      .then(response => {
         if (!response.ok) {
-          setTextoResposta(`Não foi possível listar os usuários! Erro:${response.status}`)
-          setTipoResposta("Erro")
+          throw new Error(`Erro ao buscar usuários: ${response.status}`);
         }
         return response.json();
       })
       .then((data: Usuario[]) => {
-        data.sort((a, b) => a.id - b.id);
-        setUsuarios(data);
-        const uniqueDepartamentos = new Set<string>();
-        data.forEach(usuario => {
-          const departamento = usuario.departamento;
-          if (departamento && departamento.trim() !== '') {
-            uniqueDepartamentos.add(departamento.trim());
-          }
-        });
-        setDepartamentos(Array.from(uniqueDepartamentos));
+        setUsuarios(data.sort((a, b) => a.id - b.id));
+
+        const uniqueDepartamentos = Array.from(new Set(data.map(u => u.departamento).filter(Boolean)));
+        setDepartamentos(uniqueDepartamentos);
       })
-      .catch((error) => {
-        setTextoResposta(`Erro ao processar requisição! Erro:${error}`)
-        setTipoResposta("Erro")
-      })
+      .catch(error => {
+        setTextoResposta(`Erro ao buscar usuários: ${error.message}`);
+        setTipoResposta("Erro");
+      });
   }, []);
 
-  const Pesquisando = usuarios.filter(usuario => {
-    const searchTermLower = searchTerm.toLowerCase();
+  const usuariosExibidos = mostrarInativos ? usuarios : usuarios.filter(usuario => usuario.status === 'Ativo');
 
-    if (Pesquisa === '' || Pesquisa === 'Todos') {
-      return Object.values(usuario).some(value =>
-        typeof value === 'string' && value.toLowerCase().includes(searchTermLower)
-      );
-    } else {
-      return usuario.departamento === Pesquisa &&
-        Object.values(usuario).some(value =>
-          typeof value === 'string' && value.toLowerCase().includes(searchTermLower)
-        );
-    }
+  const usuariosFiltrados = usuariosExibidos.filter(usuario => {
+    const termoBuscaMatch = Object.values(usuario).some(value =>
+      typeof value === 'string' && value.includes(termoBusca)
+    );
+    const departamentoMatch = filtroDepartamento === '' || filtroDepartamento === 'Todos' || usuario.departamento === filtroDepartamento;
+    return termoBuscaMatch && departamentoMatch;
   });
 
-
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
+    setTermoBusca(event.target.value);
   };
 
   const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setFilterValue(event.target.value);
+    setFiltroDepartamento(event.target.value);
   };
 
   const handleDelete = (id: number) => {
@@ -112,82 +96,103 @@ export default function VisualizarUsuario() {
         'Content-Type': 'application/json',
         "Authorization": token
       },
-      body: JSON.stringify({ status: 'inativo' }),
+      body: JSON.stringify({ status: 'Inativo' }),
     })
-      .then((response) => {
+      .then(response => {
         if (response.ok) {
-          setTextoResposta("Usuário excluído com sucesso!")
-          setTipoResposta("Sucesso")
+          setTextoResposta("Usuário excluído com sucesso!");
+          setTipoResposta("Sucesso");
+  
+          // Atualiza o estado removendo o usuário excluído
+          setUsuarios(prevUsuarios => prevUsuarios.filter(usuario => usuario.id !== id));
+        } else {
+          throw new Error(`Erro ao deletar usuário: ${response.status}`);
         }
-        else {
-          setTextoResposta(`Não foi possível deletar! Erro:${response.status}`)
-          setTipoResposta("Erro")
-        }
-        setUsuarios(usuarios.filter(usuario => usuario.id !== id));
       })
-      .catch((error) => {
-        setTextoResposta(`Erro ao processar requisição! Erro:${error}`)
-        setTipoResposta("Erro")
-      })
+      .catch(error => {
+        setTextoResposta(`Erro ao deletar usuário: ${error.message}`);
+        setTipoResposta("Erro");
+      });
+  };
+  
+
+  const fechaPopUp = () => {
+    setTextoResposta('');
+    setTipoResposta('');
   };
 
-  return (
-    <div className='VisualizarUsuario'>
-      <RespostaSistema textoResposta={textoResposta} tipoResposta={tipoResposta} onClose={fechaPopUp} />
-      <div>
-        <h2>Usuários</h2>
-        <select value={Pesquisa} onChange={handleFilterChange} className="mySelect">
-          <option value="">Filtro</option>
-          {departamentos.map(departamento => (
-            <option key={departamento} value={departamento}>
-              {departamento}
-            </option>
-          ))}
-        </select>
-        <input
-          type="text"
-          placeholder="Buscar por usuários"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className='myInput'
-        />
-        <table className="userTable">
-          <thead>
-            <tr></tr>
-            <tr>
-              <th className="myHeaderCell">ID</th>
-              <th className="myHeaderCell">Nome</th>
-              <th className="myHeaderCell">Departamento</th>
-              <th className="myHeaderCell">Email</th>
-              <th className="myHeaderCell">Status</th>
-              <th className="myHeaderCell">Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Pesquisando.map((usuario) => (
-              <tr key={usuario.id}>
-                <td>{usuario.id}</td>
-                <td>{usuario.nome}</td>
-                <td>{usuario.departamento}</td>
-                <td>{usuario.email}</td>
-                <td>{usuario.status}</td>
-                <td>
-                  <div className="iconContainerAtv">
-                    <Link to={`/EdicaoUsuario/${usuario.id}`}>
-                      <button type="button" className="btnIcon">
-                        <FaPencilAlt />
-                      </button>
-                    </Link>
-                    <button type="button" className="btnIcon" onClick={(event: React.MouseEvent<HTMLButtonElement>) => handleDelete(usuario.id)}>
-                      <FaTrash />
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    return (
+      <div className='VisualizarUsuario' key={chaveAtualizacao}> 
+            <RespostaSistema textoResposta={textoResposta} tipoResposta={tipoResposta} onClose={fechaPopUp} />
+            <div>
+                <h2>Usuários</h2>
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Buscar por usuários"
+                        value={termoBusca}
+                        onChange={handleSearchChange}
+                        className='myInput'
+                    />
+                    <label htmlFor="mostrarInativos" className="checkbox-label">
+                        <input 
+                            type="checkbox" 
+                            id="mostrarInativos" 
+                            checked={mostrarInativos} 
+                            onChange={() => setMostrarInativos(!mostrarInativos)} 
+                        />
+                        Mostrar Inativos
+                    </label>
+                </div>
+                <select value={filtroDepartamento} onChange={handleFilterChange} className="mySelect">
+                    <option value="">Filtro</option>
+                    <option value="Todos">Todos</option> 
+                    {departamentos.map(departamento => (
+                        <option key={departamento} value={departamento}>
+                            {departamento}
+                        </option>
+                    ))}
+                </select>
+
+                <table className="userTable">
+                    <thead>
+                        <tr></tr> 
+                        <tr>
+                            <th className="myHeaderCell">ID</th>
+                            <th className="myHeaderCell">Nome</th>
+                            <th className="myHeaderCell">Departamento</th>
+                            <th className="myHeaderCell">Email</th>
+                            <th className="myHeaderCell">Status</th>
+                            <th className="myHeaderCell">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usuariosFiltrados.map((usuario) => (
+                            <tr key={usuario.id}>
+                                <td>{usuario.id}</td>
+                                <td>{usuario.nome}</td>
+                                <td>{usuario.departamento}</td>
+                                <td>{usuario.email}</td>
+                                <td>{usuario.status} </td>
+                                <td>
+                                    <div className="iconContainerAtv">
+                                        <Link to={`/EdicaoUsuario/${usuario.id}`}>
+                                            <button type="button" className="btnIcon">
+                                                <FaPencilAlt />
+                                            </button>
+                                        </Link>
+                                        <button type="button" className="btnIcon" onClick={() => handleDelete(usuario.id)}>
+                                            <FaTrash />
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
+
+
