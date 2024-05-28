7b27074d9555784f6cf7f14deeb03b7cb141eca6
Silmara Bittencourt <silmarabittencourt13@gmail.com>
2024-05-28 11:06:19 -0300

    12.0 - feita a base da página do Manual de usuário e do css

--- ctrl-a/src/pages/manualUsuario.tsx
+++ ctrl-a/src/pages/manualUsuario.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './css/manualUsuario.css'; // Importe o arquivo CSS para estilização
+import React, { useState } from 'react';
+import './css/manualUsuario.css'; 
 
 export default function ManualUsuario() {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleAccordion = (index: number) => {
+    setActiveIndex(prevIndex => prevIndex === index ? null : index); // Usando uma função de atualização para setActiveIndex
+  };
+
+  const data = [
+    { title: 'Como fazer algo 1', content: 'Informações detalhadas sobre como fazer algo 1...' },
+    { title: 'Como fazer algo 2', content: 'Informações detalhadas sobre como fazer algo 2...' },
+    { title: 'Como fazer algo 3', content: 'Informações detalhadas sobre como fazer algo 3...' },
+    // Adicione quantos itens desejar
+  ];
+
   return (
     <div className="manual-container">
-        <div className='TituloManual'>
-            <header>
-                <h1>Manual do Usuário</h1>
-            </header>
-        </div>
-      
+      <div className='TituloManual'>
+        <header>
+          <h1>Manual do Usuário</h1>
+        </header>
+      </div>
+      <div className="accordion">
+        {data.map((item, index: number) => (
+          <div key={index} className="accordion-item">
+            <div
+              className={`accordion-title ${activeIndex === index ? 'active' : ''}`}
+              onClick={() => toggleAccordion(index)}
+            >
+              {item.title}
+            </div>
+            {activeIndex === index && (
+              <div className="accordion-content">
+                {item.content}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
