2e47b1a779f44a61add8723c6e32e58a8186efab
BrunoSerpa <serpabruno2005@gmail.com>
2024-06-05 17:58:16 -0300

    9 - Arrumei a estrutura do campo de enviar Arquivo. inseri um botão de limpar o arquivo inserido

--- ctrl-a/src/components/CampoArquivo.tsx
+++ ctrl-a/src/components/CampoArquivo.tsx
@@ -1,77 +1,105 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./css/CampoPadrao.css";
-
-type arquivo = {
-    "nome": string,
-    "tipoDocumento": string,
-    "documento": number[]
-}
 
 export default function CampoArquivo(
     titulo: string,
+    tipos: string[],
+    placeholder: string,
+    tamanhoMB: number,
     obrigatorio: boolean,
     aviso?: string,
-    aceitos?: string[]
 ) {
-    const [notaFiscal, setNotaFiscal] = useState({
-        "nome": "",
-        "tipoDocumento": "",
-        "documento": []
-    } as arquivo);
+    const [arquivo, setArquivo] = useState<{ nome: string, tipoArquivo: string, documento: number[] }>({
+        nome: '',
+        tipoArquivo: '',
+        documento: []
+    })
+    const [tamanho, setTamanho] = useState(0)
     const [erroCampo, setErroCampo] = useState(false);
+    useEffect(() => {
+        aviso && setAvisoArquivo(aviso)
+    }, [aviso])
 
     const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        const arquivos = e.target.files;
-        if (!arquivos || arquivos?.length < 1) return;
-
-        const nome = arquivos[0].name;
-        const tipoDocumento = arquivos[0].type;
-        const documento = await arquivos[0].arrayBuffer();
-        
-        setNotaFiscal({
-            "nome": nome,
-            "tipoDocumento": tipoDocumento,
-            "documento": Array.from(new Uint8Array(documento))
-        } as arquivo);
-        if (erroCampo) {
-            setErroCampo(false);
-        }
-    };
-
-    const handleBlur = () => {
-        if (!notaFiscal.nome && obrigatorio) {
-            setErroCampo(true);
-        }
-    };
-
-    function limpar() {
-        setNotaFiscal({
-            "nome": "",
-            "tipoDocumento": "",
-            "documento": []
-        } as arquivo);
+        const arquivoRecebido = e.target.files;
+        if (!arquivoRecebido || arquivoRecebido.length < 1) {
+            return;
+        };
+        const documento = await arquivoRecebido[0].arrayBuffer();
+        setTamanho(arquivoRecebido[0].size)
+        setArquivo({
+            nome: arquivoRecebido[0].name,
+            tipoArquivo: arquivoRecebido[0].type,
+            documento: Array.from(new Uint8Array(documento))
+        });
     }
 
+    function limpar() {
+        setArquivo({
+            nome: '',
+            tipoArquivo: '',
+            documento: []
+        });
+        setTamanho(0)
+    }
+
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const handleInputClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
+    const [avisoArquivo, setAvisoArquivo] = useState('')
+    useEffect(() => {
+        const arq = arquivo.nome.split('.')
+        if (tamanho > (tamanhoMB * 1000000)) {
+            setErroCampo(true);
+            setAvisoArquivo(`O arquivo é maior do que ${tamanhoMB} MB`)
+        } else if (arquivo.nome !== '' && !tipos.includes('.' + (arq[arq.length - 1]))) {
+            setErroCampo(true);
+            setAvisoArquivo(`O tipo do arquivo não é válido! `)
+        } else if (erroCampo) {
+            setErroCampo(false);
+        }
+    }, [arquivo])
+
     const codigo = (
-        <div className={`divCampoSenha  ${erroCampo ? 'alertaCampoErrado' : 'semAlertaCampo'}`}>
-            <div>
-                <span>{titulo} {obrigatorio && <span className="inputObrigatorio">*</span>}</span>
-                <div className="inputCampoSenha">
-                    <input
-                        type='file'
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        accept={aceitos?.join(",")}
-                    ></input>
+        <div className='divCampoArquivo'>
+            <span>{titulo} {obrigatorio && <span className="inputObrigatorio">*</span>}</span>
+            <div className="campoArquivo">
+                {arquivo.tipoArquivo !== '' ?
+                    <span>
+                        {`${arquivo.nome} (${(tamanho / 1000000).toFixed(2)} MB)`}
+                        <button
+                            className="botaoRemoverArquivo"
+                            onClick={() => limpar()}    
+                        >X</button>
+                    </span> :
+                    <span>Nenhum arquivo inserido</span>
+                }
+                <input
+                    type='file'
+                    accept={tipos?.join(",")}
+                    onChange={handleChange}
+                    ref={fileInputRef}
+                />
+                <div>
+                    <div
+                        className="controleInputArquivo"
+                        onClick={handleInputClick}
+                    >{placeholder}</div>
+                    <span>{`Do tipo: ${tipos.join(", ").replace(/\./g, '')}.`}</span>
                 </div>
+                {erroCampo && avisoArquivo && <span className="erroCampo">{avisoArquivo}</span>}
             </div>
-            {aviso && erroCampo && <span className="erroCampo">{aviso}</span>}
         </div>
     );
 
     return {
-        dado: notaFiscal,
+        dado: arquivo,
         codigo,
-        limpar
+        limpar,
+        erroCampo
     };
 }
